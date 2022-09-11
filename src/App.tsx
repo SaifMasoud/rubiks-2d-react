3509cb274b0fe50b6698c54e0c79ac55c7c10cf3
Saif Masoud <saifemasoud@gmail.com>
2022-09-11 14:10:44 -0400

    <select> dropdown for position list

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import Cubie from "./Cubie";
 import Position from "./Position";
 
 function App() {
-  const [pos, setPos] = useState(Array.from(Array(24).keys()));
+  const [posList, setPosList] = useState([Array.from(Array(24).keys()), [13, 14, 12, 3, 4, 5, 2, 0, 1, 9, 10, 11, 20, 18, 19, 15, 16, 17, 7,8, 6, 21, 22, 23,]])
+  const [posListIndex, setPosListIndex] = useState(0)
   return (
     <div className="App">
-      <Position pos={pos}></Position>
+      <Position pos={posList[posListIndex]}></Position>
       <button
         onClick={() =>
-          setPos([
-            13, 14, 12, 3, 4, 5, 2, 0, 1, 9, 10, 11, 20, 18, 19, 15, 16, 17, 7,
-            8, 6, 21, 22, 23,
-          ])
+          setPosListIndex((posListIndex + 1) % posList.length)
         }
       >
         POSITION CHANGE
       </button>
+      <select onChange={(event) => setPosListIndex(parseInt(event.target.value))}>
+        {posList.map((pos, i) => <option value={i}>{pos}</option>)}
+      </select>
     </div>
   );
 }
