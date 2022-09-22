c5a12726305d16c61f2ed6985849f07cbf07cde1
Saif Masoud <saifemasoud@gmail.com>
2022-09-22 11:46:10 -0400

    Added index route for flask
    So we can deploy from ./build using just flask server

--- api/app.py
+++ api/app.py
@@ -2,11 +2,15 @@ from flask import Flask, request, app
 import rubik
 from bidirectinoal_bfs import bidirectional_bfs_search, rubik_list_states
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./build", static_url_path='/')
+
+@app.route('/')
+def index():    
+    return app.send_static_file('index.html')
 
 
 @app.route("/api", methods=["POST"])
-def index():
+def solve():
     colors = request.json.get('colors_list')
     try:
         perm = ui_color_arr_to_permutation(colors)
